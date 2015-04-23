ac9b646dca3d1347d99be3ba3873384f480b63fb
Helen <heleny@princeton.edu>
2015-04-23 03:24:44 -0400

    preprocessing code for lda-c

---
+++ lda_c.py
@@ -0,0 +1,25 @@
+f = open('txTripletsCounts.txt', 'r')
+f_out = open('lda_c_counts.txt', 'w')
+prev_doc = 0
+triplets = f.readlines()
+line = ""
+count = 0
+for l in triplets:
+	triplet = l.split()
+	current = int(triplet[0])
+	if (current != prev_doc):
+		f_out.write(str(count) + " " + line + "\n")
+		prev_doc += 1
+		while (prev_doc != current):
+			f_out.write("0\n") # for senders who do not send anything
+			prev_doc+=1
+		line = ""
+		count = 0
+	line += " " + triplet[1] + ":" + triplet[2]
+	count += int(triplet[2])
+
+f_out.write(str(count) + " " + line)
+
+f.close()
+f_out.close()
+
