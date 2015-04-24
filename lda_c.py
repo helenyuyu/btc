5723643d2373c0ab2b34fbb94ae6f11c769c2089
Helen <heleny@princeton.edu>
2015-04-23 22:23:06 -0400

    reduced data set

--- lda_c.py
+++ lda_c.py
@@ -1,4 +1,4 @@
-f = open('txTripletsCounts.txt', 'r')
+f = open('reduced_txTripletsCounts.txt', 'r')
 f_out = open('lda_c_counts.txt', 'w')
 prev_doc = 0
 triplets = f.readlines()
@@ -8,7 +8,7 @@ for l in triplets:
 	triplet = l.split()
 	current = int(triplet[0])
 	if (current != prev_doc):
-		f_out.write(str(count) + " " + line + "\n")
+		f_out.write(str(count) + line + "\n")
 		prev_doc += 1
 		while (prev_doc != current):
 			f_out.write("0\n") # for senders who do not send anything
