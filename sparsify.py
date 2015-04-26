5f4f9b75bf25cc6dbaeaa2b191566c29fadf1a0c
Helen <heleny@princeton.edu>
2015-04-25 22:45:35 -0400

    added lda

--- sparsify.py
+++ sparsify.py
@@ -5,29 +5,34 @@ from sklearn.metrics import matthews_corrcoef, roc_auc_score, roc_curve, auc
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 from functools import partial
+import math
+from lda import LDA
 
+num_users = 444075
 f = open('txTripletsCounts.txt', 'r')
 triplets = f.readlines()
 triplets = np.array([l.split() for l in triplets])
 row = [int(s) for s in triplets[:,0]]
 col = [int(s) for s in triplets[:,1]]
 data = [int(s) for s in triplets[:,2]]
-bin_data = [1 for s in triplets[:,2]]
-num_users = 444075
-X = sparse.csc_matrix((bin_data, (row,col)), shape=(num_users, num_users))
+binary = False
+if binary:
+  data = [1 for s in triplets[:,2]]
+X = sparse.csc_matrix((data, (row,col)), shape=(num_users, num_users))
 f.close()
-#cov = x * csc_matrix.transpose(x)
 
 
 def nmf():
   name = "nmf"
   model = NMF(n_components = 25)
-  print name
+  if binary:
+    print "binary", name
+  else:
+    print "counts", name
   model.fit(X)
   A_T = sparse.csc_matrix(model.components_)
   Z = A_T*X
   A = A_T.transpose()
-  print "reconstruction error", model.reconstruction_err_
   def get_prob(i, j):
     return (A.getrow(i)*Z.getcol(j))[0,0]
   display_results(name, get_prob) 
@@ -35,8 +40,11 @@ def nmf():
 
 def svd():
   name = "svd"
+  if binary:
+    print "binary", name
+  else:
+    print "counts", name
   model = TruncatedSVD(n_components = 25)
-  print name
   model.fit(X)
   print(model.explained_variance_ratio_)
   print "total explained variance", model.explained_variance_ratio_.sum()
@@ -51,29 +59,74 @@ def svd():
 
 def kmeans():
   name = "kmeans"
+  if binary:
+    print "binary", name
+  else:
+    print "counts", name
   model = KMeans()
-  print name
   labels = model.fit_predict(X)
   def get_prob(i,j):
     return model.cluster_centers_[labels[i]][j]
   display_results(name, get_prob)
 
+def lda():
+  name = "lda_py"
+  n_topics = 20
+  model = LDA(n_topics = n_topics, n_iter = 100)
+  model.fit(X)
+  distributions = model.components_
+  doc_topic = model.doc_topic_
+  def get_prob(i,j):
+    sum = 0
+    for topic in range(0, n_topics):
+      sum += distributions[topic][j]*doc_topic[i][topic] 
+    return sum
+
+  display_results(name, get_prob)
+  # num_labels = 10
+  # name = "lda"
+  # f_distributions = open('final10.beta', 'r')
+  # f_labels = open('word-assignments10.dat', 'r')
+  # all_labels = []
+  # distributions = []
+  # for l in f_distributions:
+  #   distribution = l.split()
+  #   distribution = [math.exp(float(i)) for i in distribution]
+  #   #fill in 10 missing users at end
+  #   for i in range(0, num_labels):
+  #     distribution.append(0)
+  #   distributions.append(distribution)
+
+
+  # for l in f_labels:
+  #   labels = [0] * num_labels
+  #   l = l.split()
+  #   for pair in l[1:]:
+  #     pair = pair.split(":")
+  #     topic = int(pair[1])
+  #     labels[topic]+=1
+  #   all_labels.append(labels)
+  
+
+  # f_labels.close()
+  # f_distributions.close()
+
+  # def get_prob(i,j):
+  #   sum = 0
+  #   count = 0
+  #   for label in range(0, num_labels) :
+  #     sum+= distributions[label][j]*all_labels[i][label]
+  #     count+= all_labels[i][label]
+  #   if count == 0:
+  #    return 0
+  #   return sum / count
+  # display_results(name, get_prob)
 
 
 def display_results(name, get_prob):
   f_test = open('testTriplets.txt', 'r')
+  f_out = open(name+"Probs.txt", "w")
   test = np.array([l.split() for l in f_test.readlines()])
-  tp = 0
-  tps = []
-  tn = 0
-  tns = []
-  fp = 0
-  fps = []
-  fn = 0
-  fns = []
-  count = 0
-  cutoff = 1e-3
-  bin_preds = []
   probs = []
   trues = [] 
   for l in test: 
@@ -81,54 +134,36 @@ def display_results(name, get_prob):
       j = int(l[1])
       value = int(l[2])
 
-      pred = get_prob(i,j)
-      prob = pred
-      if pred > 1 :
+      prob = get_prob(i,j)
+      if prob > 1 :
           prob = 1
-      elif pred < 0:
+      elif prob < 0:
           prob = 0
-
-      if pred > cutoff: 
-          bin_pred = 1
-      else:
-          bin_pred = 0
-
-      if value == 0 and bin_pred == value:
-          tn += 1
-      if value == 1 and bin_pred == value:
-          tp += 1
-      if value == 0 and bin_pred != value:
-          fp += 1
-      if value == 1 and bin_pred != value:
-          fn += 1
-
+      f_out.write(str(prob)+"\n")
       trues.append(value)
-      bin_preds.append(bin_pred)
       probs.append(prob)
   f_test.close()
-  print "tp", tp
-  print "tn", tn
-  print "fp", fp
-  print "fn", fn
+  f_out.close()
 
-  print "matthews correlation", matthews_corrcoef(trues, bin_preds)
 
-  fpr, tpr, thresholds = roc_curve(trues, probs, pos_label =1)
-  roc_auc = auc(fpr, tpr)
-  print "auc", roc_auc 
-  plt.figure()
-  plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
-  plt.plot([0, 1], [0, 1], 'k--')
-  plt.xlim([0.0, 1.0])
-  plt.ylim([0.0, 1.0])
-  plt.xlabel('False Positive Rate')
-  plt.ylabel('True Positive Rate')
-  plt.title(name + ' ROC curve')
-  plt.legend(loc="lower right")
-  plt.show()
+
+  # fpr, tpr, thresholds = roc_curve(trues, probs, pos_label =1)
+  # roc_auc = auc(fpr, tpr)
+  # print "auc", roc_auc 
+  # plt.figure()
+  # plt.plot(fpr, tpr, label='ROC curve (area = %0.2f)' % roc_auc)
+  # plt.plot([0, 1], [0, 1], 'k--')
+  # plt.xlim([0.0, 1.0])
+  # plt.ylim([0.0, 1.0])
+  # plt.xlabel('False Positive Rate')
+  # plt.ylabel('True Positive Rate')
+  # plt.title(name + ' ROC curve')
+  # plt.legend(loc="lower right")
+  # plt.show()
 
 
 
 #nmf()
-svd()
+#svd()
 #kmeans()
+lda()
